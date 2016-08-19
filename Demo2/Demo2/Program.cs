1e73223e7620172750037880548c1edc546d0068
gerson.barrantes <gerbarr1303@gmail.com>
2016-08-18 19:07:52 -0600

    Inicio Proyecto

--- Demo2/Demo2/Program.cs
+++ Demo2/Demo2/Program.cs
@@ -6,10 +6,56 @@ using System.Threading.Tasks;
 
 namespace Demo2
 {
+
     class Program
     {
+        /// <summary>          
+        /// </summary>
+        /// 
         static void Main(string[] args)
         {
+            Console.Title = "Clima";
+
+            bool continuar = true;
+
+            while (continuar)
+            {
+                Console.Clear();
+
+                Console.WriteLine("Seleccione una operación: ");
+
+                Console.WriteLine("1. Farenheit to Celsios");
+                Console.WriteLine("2. Celsios to Farenheit");
+
+
+                string opcion = Console.ReadLine();
+
+                if (opcion == "1")
+                {
+                    ConvertCelsiostoFarenheit();
+                }
+
+                if (opcion == "2")
+                {
+                    ConvertFarenheittoCelsios();
+                }
+
+
+                Console.WriteLine("\nSeleccione una opción: ");
+                Console.WriteLine("1. Volver al menú principal");
+                Console.WriteLine("2. Salir");
+
+                opcion = Console.ReadLine();
+
+                if (opcion != "1")
+                {
+                    continuar = false;
+                }
+
+            }
         }
     }
 }
+                
+            
+
