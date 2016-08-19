01196fc76eaf4c35c263d00fc92561e30d741216
Krosales14 <dondekenito@gmail.com>
2016-08-18 19:05:23 -0600

    Calculo

--- Demo2/Demo2/Program.cs
+++ Demo2/Demo2/Program.cs
@@ -12,13 +12,32 @@ namespace Demo2
         {
 
         }
-
-        private double ConvertCelsiostToFarenheit(double celsios) {
-            return celsios * 1.8 + 32;
+        
+        private void ConvertCelsiostToFarenheit()
+        {
+            Console.Clear();
+            Console.WriteLine("Convertir a Farenheit");
+            Console.WriteLine("");
+            Console.WriteLine("Ingrese el primer Valor");
+            double celsios = double.Parse(Console.ReadLine());
+            double farenheit = celsios * 1.8 + 32;
+            Console.WriteLine("El resultado es: " + farenheit);          
+            ///return * 1.8 + 32;
         }
-
-        private double ConvertFarenheitToCelsios(double Farenheit) {
-            return Farenheit / 1.8 - 32;
+        private void ConvertFarenheitToCelsios()
+        {
+            Console.Clear();
+            Console.WriteLine("Convertir a Farenheit");
+            Console.WriteLine("");
+            Console.WriteLine("Ingrese el primer Valor");
+            double farenheit = double.Parse(Console.ReadLine());
+            double celsios = farenheit / 1.8 - 32;
+            Console.WriteLine("El resultado es: " + farenheit);
+            ///return * 1.8 + 32;
         }
+        /// private double ConvertFarenheitToCelsios(double Farenheit) {
+            ///return Farenheit / 1.8 - 32;
+///
+      ///  }
     }
 }
